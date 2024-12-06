a6a34f5e86eff624290ec26cbc8641a611384d6d
park-ria <danny6217@gmail.com>
2024-12-06 18:07:51 +0900

    finish header

--- src/components/UserBox.tsx
+++ src/components/UserBox.tsx
@@ -10,6 +10,8 @@ const UserArea = styled.div<{ $position: string }>`
   width: 32px;
   height: 32px;
   border-radius: 50%;
+  position: relative;
+  z-index: 11;
   overflow: hidden;
   @media screen and (max-width: 768px) {
     display: ${({ $position }) => ($position === "top" ? "none" : "block")};
@@ -24,12 +26,13 @@ const UserImg = styled.img`
 
 const UserNav = styled.ul<{ $position: string; $isOpen: boolean }>`
   width: 100px;
-  padding: 10px 0;
+  padding: 5px 0;
   border-radius: 5px;
   text-align: center;
   background: #666;
   position: absolute;
-  ${({ $position }) => ($position === "top" ? "bottom:-50px" : "top:-50px")};
+  z-index: 11;
+  ${({ $position }) => ($position === "top" ? "bottom:-40px" : "top:-40px")};
   right: -5px;
   display: ${({ $isOpen }) => ($isOpen ? "block" : "none")};
 `;
@@ -43,6 +46,18 @@ const Logout = styled.span`
   }
 `;
 
+const Overlay = styled.div<{ $isOpen: boolean }>`
+  display: ${({ $isOpen }) => ($isOpen ? "block" : "none")};
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  cursor: pointer;
+  background: transparent;
+`;
+
 const UserBox = ({ position }: { position: string }) => {
   const [isOpen, setIsOpen] = useState(false);
   const navigation = useNavigate();
@@ -54,6 +69,7 @@ const UserBox = ({ position }: { position: string }) => {
       <UserNav $position={position} $isOpen={isOpen}>
         <Logout onClick={() => navigation("/login")}>로그아웃</Logout>
       </UserNav>
+      <Overlay $isOpen={isOpen} onClick={() => setIsOpen(false)} />
     </Wrapper>
   );
 };
